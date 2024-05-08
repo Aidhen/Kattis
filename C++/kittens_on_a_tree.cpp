9aec8021061aee35f1024aa729a8e60b78f9a871
Aidhen <94020294+Aidhen@users.noreply.github.com>
2024-05-09 01:48:00 +0200

    delete useless comments

--- C++/kittens_on_a_tree.cpp
+++ C++/kittens_on_a_tree.cpp
@@ -4,7 +4,7 @@
 #include<sstream>
 using namespace std; 
 
-/*Takes a row of integers and puts them into a vector in order*/
+
 vector<int> ExtraxtInt(string &line){
     vector<int> numbers;
     stringstream ss(line);
@@ -38,18 +38,11 @@ int main(void){
         if(line == "-1"){break;}
         branches.push_back(ExtraxtInt(line));    
     }
-        
-    /*cout << "kitten = " << kitten_position << "\n"; 
-    for(int i = 0; i < branches.size(); i++){
-        string label = "branch " + to_string(i);
-        print(label, branches[i]);
-    }*/
     
     int path = kitten_position;
     int root = branches[1][0];
     string complete_path = to_string(path) + " ";
     int i = 1;
-    //cout << branches.size();
     while(path != root){
         int tmp = branches[i][0];
         for(int j = 1; j < branches[i].size(); j++){
@@ -59,11 +52,7 @@ int main(void){
                 i = 0;                
             }
         }
-        /*if(i == branches.size() - 1){
-            cout << "There isn't path\n";
-            path = root;
-            break;
-        }*/
+        
         i++;
     }
     cout << complete_path << "\n";
